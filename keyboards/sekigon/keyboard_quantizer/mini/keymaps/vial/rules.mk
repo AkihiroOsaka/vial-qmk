9c71f06cac2137df0e3c779d7ae256877bafd9d0
meme <osino.akihiro@gmail.com>
2026-03-03 22:52:24 +0900

    Update rules.mk

--- keyboards/sekigon/keyboard_quantizer/mini/keymaps/vial/rules.mk
+++ keyboards/sekigon/keyboard_quantizer/mini/keymaps/vial/rules.mk
@@ -1,7 +1,7 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIAL_INSECURE = yes
-# ↓ マウス・スクロール機能を有効化するために必須の1行です
+# マウス・スクロール機能を有効化
 POINTING_DEVICE_ENABLE = yes
 
 SRC += quantizer_mouse.c raw_hid.c
@@ -14,7 +14,7 @@ VPATH += keyboards/sekigon/keyboard_quantizer/mini/keymaps/vial/key_override
 GIT_DESCRIBE := $(shell git describe --tags --long --dirty="\\*" 2>/dev/null)
 CFLAGS += -DGIT_DESCRIBE=$(GIT_DESCRIBE)
 
-# Override raw_hid_receive to support both of VIA and VIAL
-$(BUILD_DIR)/obj_sekigon_keyboard_quantizer_mini_vial/quantum/via.o:: CFLAGS += -Draw_hid_receive=raw_hid_receive_vial
+# ↓ ここを修正：via.o にもマクロ送信関数の書き換え（_vial）を適用します
+$(BUILD_DIR)/obj_sekigon_keyboard_quantizer_mini_vial/quantum/via.o:: CFLAGS += -Draw_hid_receive=raw_hid_receive_vial -Ddynamic_keymap_macro_send=dynamic_keymap_macro_send_vial
 $(BUILD_DIR)/obj_sekigon_keyboard_quantizer_mini_vial/quantum/dynamic_keymap.o:: CFLAGS += -Ddynamic_keymap_macro_send=dynamic_keymap_macro_send_vial
 SRC += tmk_core/protocol/bmp/via_qmk.c
